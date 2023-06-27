61d6882db9f5d046ca82e98d14a98debb4ec71da
Ayodele Similoluwa <similoluwaayoayodele@gmail.com>
2023-06-27 14:18:31 +0100

    changes to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,25 +1,24 @@
 import Trailerpoint from "../assets/portfolio/Trailerpoint.png";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-
+import FitnessJunkie from "../assets/portfolio/FitnessJunkie.png";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
       src: Trailerpoint,
+      href: "https://trailerpoint.netlify.app",
+      code: "https://github.com/UncleSims/TrailerPoint",
     },
     {
       id: 2,
-      src: installNode,
+      src: FitnessJunkie,
+      href: "https://fitnessjunkie.netlify.app/",
+      code: "https://github.com/UncleSims/gym-project",
     },
-    {
-      id: 3,
-      src: navbar,
-    },
-   
-
-   
+    // {
+    //   id: 3,
+    //   src: navbar,
+    // },
   ];
   return (
     <section
@@ -31,12 +30,18 @@ const Portfolio = () => {
           <span className="text-3xl font-bold border-b-4 border-gray-500">
             Portfolio
           </span>
-          <p className="pt-6 text-gray-400">Here are a few of my works, feel free to check them out.</p>
+          <p className="pt-6 text-gray-400">
+            Here are a few of my works, feel free to check them out.
+          </p>
         </div>
 
-        <div className="h-2/5 grid sm:grid-cols-2 md:grid-cols-3 gap-8 cntent-center px-12  sm:px-0" >
-          {portfolios.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg" data-aos="flip-left">
+        <div className="h-2/5 grid sm:grid-cols-2 md:grid-cols-2 gap-8 cntent-center px-12  sm:px-0">
+          {portfolios.map(({ id, src, href, code }) => (
+            <div
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg"
+              data-aos="zoom-in"
+            >
               <img
                 className="rounded-md duration-200 hover:scale-105"
                 src={src}
@@ -44,10 +49,14 @@ const Portfolio = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
+                  <a href={href} target="_blank">
+                    Demo
+                  </a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                  <a href={code} target="_blank">
+                    Code
+                  </a>
                 </button>
               </div>
             </div>
